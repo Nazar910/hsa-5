d151fc49fe5243b85167d6850c94b80bb4bf77ab
Nazarii Pyvovar <Nazar910>
2021-12-04 12:28:57 +0200

    L8: dockerify rest-app

--- L8/rest-app/src/knex.ts
+++ L8/rest-app/src/knex.ts
@@ -1,12 +1,15 @@
 import { knex as createKnex } from 'knex';
 
+const { MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } =
+  process.env;
+
 export const knex = createKnex({
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: 'example',
-    database: 'l8_db',
+    host: MYSQL_HOST || '127.0.0.1',
+    port: Number(MYSQL_PORT) || 3306,
+    user: MYSQL_USER || 'root',
+    password: MYSQL_PASSWORD || 'example',
+    database: MYSQL_DATABASE || 'l8_db',
   },
 });
