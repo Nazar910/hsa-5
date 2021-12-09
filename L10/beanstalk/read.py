5d534041fec810fa73a571ae9cfb7c627da38d4b
Nazarii Pyvovar <Nazar910>
2021-12-09 18:11:24 +0200

    L10: upd read command to watch tube

--- L10/beanstalk/read.py
+++ L10/beanstalk/read.py
@@ -2,11 +2,9 @@ import greenstalk
 import json
 
 with greenstalk.Client(('127.0.0.1', '11300')) as client:
-    client.use('foo_bar')
+    client.watch('foo_bar')
 
     while True:
-        print('about to reserve')
         job = client.reserve()
-        print('Got job')
         print(job.body)
         client.delete(job)
--- L10/beanstalk/write.py
+++ L10/beanstalk/write.py
@@ -4,6 +4,6 @@ import json
 with greenstalk.Client(('127.0.0.1', '11300')) as client:
     client.use('foo_bar')
 
-    msg = json.dumps({ 'foo': 'bar' })
-    client.put(msg)
-    print('Success')
+    for i in range(0, 100):
+        msg = json.dumps({ 'foo': i })
+        client.put(msg)
