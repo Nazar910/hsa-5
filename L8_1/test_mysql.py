bf42a7d19f7e8f55eb935077292497359f603445
Nazarii Pyvovar <Nazar910>
2021-12-14 23:10:54 +0200

    L8.1: scratch MySQL

--- L8_1/test_mysql.py
+++ L8_1/test_mysql.py
@@ -1,48 +1,75 @@
-# from mysql.connector import connect
-
 import pymysql
 import pymysql.cursors
 
+
 def get_connection():
-    con = pymysql.connect(host='localhost',
+    con = pymysql.connect(
+        host='localhost',
         user='root',
         password='example',
         db='l8_1_db',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor)
+        charset='utf8mb4'
+    )
+
     con.autocommit = False
 
     return con
+
 
 def ensure_tables():
     con = get_connection()
     with con:
         con.query("""
-            CREATE TABLE IF NOT EXISTS tabl1 (
+            CREATE TABLE IF NOT EXISTS tbl1 (
                 id INT NOT NULL AUTO_INCREMENT,
                 f1 INT NOT NULL,
                 f2 INT NOT NULL,
                 PRIMARY KEY (id)
             )
         """)
-        con.query('INSERT INTO tabl1 (f1, f2) VALUES (1, 0)')
+        con.query('SET autocommit=0')
+        con.query('SET GLOBAL innodb_status_output=ON')
+        con.query('SET GLOBAL innodb_status_output_locks=ON')
         con.commit()
+
+
+def ensure_data():
+    con = get_connection()
+    with con:
+        con.query('DELETE FROM tbl1')
+        con.query('INSERT INTO tbl1 (f1, f2) VALUES (1, 0)')
+        con.query('INSERT INTO tbl1 (f1, f2) VALUES (2, 0)')
+        con.commit()
+
 
 def lost_update():
     con1 = get_connection()
     con2 = get_connection()
 
-    con1.begin()
-    con1.query('UPDATE tbl1 SET f2=f2+20 WHERE f1=1;')
+    cursor1 = con1.cursor()
+    cursor2 = con2.cursor()
 
+    con1.begin()
     con2.begin()
-    con2.query('UPDATE tbl1 SET f2=f2+25 WHERE f1=1;')
+
+    print('Got cursors')
+
+    cursor1.execute('UPDATE tbl1 SET f2=f2+20 WHERE f1=1;')
+    print('cursor1.execute')
+
+    cursor2.execute('UPDATE tbl1 SET f2=f2+25 WHERE f1=1;')
+    print('cursor2.execute')
 
     con2.commit()
+    print('con2.commit')
 
     con1.commit()
+    print('con1.commit')
 
+    cursor1.close()
     con1.close()
+
+    cursor2.close()
     con2.close()
 
     con = get_connection()
@@ -50,26 +77,32 @@ def lost_update():
     with con:
         cursor = con.cursor()
 
-        cursor.execute('SELECT * FROM tabl1')
+        cursor.execute('SELECT * FROM tbl1')
         results = cursor.fetchall()
         print(results)
 
+
 TRANSACTION_LVLS = [
-    'READ UNCOMMITED',
-    'READ COMMITED',
-    'REPEATABLE_READ',
-    'SERILIZABLE'
+    'READ UNCOMMITTED',
+    'READ COMMITTED',
+    'REPEATABLE READ',
+    'SERIALIZABLE',
 ]
 
-def cases():
-    for case in TRANSACTION_LVLS:
+
+def test_levels():
+    ensure_tables()
+    for level in TRANSACTION_LVLS:
+        ensure_data()
+        print('Level: %s' % level)
         with get_connection() as con:
-            con.query('SET TRANSACTION ISOLATION LEVEL %s' % case)
+            with con.cursor() as cursor:
+                cursor.execute(
+                    'SET GLOBAL TRANSACTION ISOLATION LEVEL %s' % level)
+
             con.commit()
 
         lost_update()
 
 
-
-ensure_tables()
-cases()
+test_levels()
