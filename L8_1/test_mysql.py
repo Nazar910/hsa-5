6b61f47b94662b841af313f9112d1bc30c469144
Nazarii Pyvovar <Nazar910>
2021-12-15 22:41:15 +0200

    L8.1: finished MySQL isolation test cases

--- L8_1/test_mysql.py
+++ L8_1/test_mysql.py
@@ -1,6 +1,6 @@
 import pymysql
 import pymysql.cursors
-from multiprocessing import Process, process
+from multiprocessing import Process
 import time
 
 
@@ -10,7 +10,8 @@ def get_connection():
         user='root',
         password='example',
         db='l8_1_db',
-        charset='utf8mb4'
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
     )
 
     con.autocommit = False
@@ -53,10 +54,14 @@ def print_table():
             results = cursor.fetchall()
             print(results)
 
+
 def lost_update():
+    print('==========================LOST UPDATE========================')
+
     def session1():
         con = get_connection()
         with con:
+            con.begin()
             with con.cursor() as cursor:
                 cursor.execute('UPDATE tbl1 SET f2=f2+20 WHERE f1=1;')
                 print('session1: update finished')
@@ -65,6 +70,7 @@ def lost_update():
     def session2():
         con = get_connection()
         with con:
+            con.begin()
             with con.cursor() as cursor:
                 cursor.execute('UPDATE tbl1 SET f2=f2+25 WHERE f1=1;')
                 print('session2: update finished')
@@ -76,37 +82,35 @@ def lost_update():
     p2.start()
     p1.join()
     p2.join()
-
-    con = get_connection()
-
-    with con:
-        cursor = con.cursor()
-
-        cursor.execute('SELECT * FROM tbl1')
-        results = cursor.fetchall()
-        print(results)
+    print('=============================================================')
 
 
 def dirty_read():
+    print('==========================DIRTY READ========================')
+
     def session1():
         con = get_connection()
         with con:
+            con.begin()
             with con.cursor() as cursor:
                 cursor.execute('SELECT f2 FROM tbl1 WHERE f1=1')
                 print('session1: select finished % s' % cursor.fetchmany())
                 cursor.execute('UPDATE tbl1 SET f2=f2+1 WHERE f1=1')
                 print('session1: update finished')
             time.sleep(2)
-            con.commit()
-            print('session 1: commit')
+            con.rollback()
+            print('session 1: rollback')
 
     def session2():
         con = get_connection()
         with con:
+            con.begin()
             time.sleep(1)
             with con.cursor() as cursor:
                 cursor.execute('SELECT f2 FROM tbl1 WHERE f1=1')
-                print('session2: select finished % s' % cursor.fetchmany())
+                print('session 2: select finished % s' % cursor.fetchmany())
+            con.commit()
+            print('session 2: commit')
 
     p1 = Process(target=session1)
     p1.start()
@@ -114,15 +118,84 @@ def dirty_read():
     p2.start()
     p1.join()
     p2.join()
+    print('=============================================================')
 
-    con = get_connection()
 
-    with con:
-        cursor = con.cursor()
+def non_repeatable_read():
+    print('======================NON REPEATABLE READ======================')
 
-        cursor.execute('SELECT * FROM tbl1')
-        results = cursor.fetchall()
-        print(results)
+    def session1():
+        con = get_connection()
+        with con:
+            con.begin()
+            with con.cursor() as cursor:
+                cursor.execute('SELECT f2 FROM tbl1 WHERE f1=1')
+                print('session 1: select finished % s' % cursor.fetchmany())
+                cursor.execute('UPDATE tbl1 SET f2=f2+1 WHERE f1=1')
+                print('session 1: update finished')
+            con.commit()
+            print('session 1: commit')
+
+    def session2():
+        con = get_connection()
+        with con:
+            con.begin()
+            with con.cursor() as cursor:
+                cursor.execute('SELECT f2 FROM tbl1 WHERE f1=1')
+                print('session 2: first select finished % s' %
+                      cursor.fetchmany())
+                time.sleep(2)
+                cursor.execute('SELECT f2 FROM tbl1 WHERE f1=1')
+                print('session 2: second select finished % s' %
+                      cursor.fetchmany())
+            con.commit()
+            print('session 2: commit')
+
+    p1 = Process(target=session1)
+    p1.start()
+    p2 = Process(target=session2)
+    p2.start()
+    p1.join()
+    p2.join()
+    print('=============================================================')
+
+
+def phantom_read():
+    print('======================PHANTOM READ======================')
+
+    def session1():
+        con = get_connection()
+        with con:
+            con.begin()
+            time.sleep(1)
+            with con.cursor() as cursor:
+                cursor.execute('INSERT INTO tbl1 (f1, f2) VALUES (15,20)')
+                print('session 1: insert finished')
+            con.commit()
+            print('session 1: commit')
+
+    def session2():
+        con = get_connection()
+        with con:
+            con.begin()
+            with con.cursor() as cursor:
+                cursor.execute('SELECT sum(f2) FROM tbl1')
+                print('session 2: first select finished % s' %
+                      cursor.fetchmany())
+                time.sleep(2)
+                cursor.execute('SELECT sum(f2) FROM tbl1')
+                print('session 2: second select finished % s' %
+                      cursor.fetchmany())
+            con.commit()
+            print('session 2: commit')
+
+    p1 = Process(target=session1)
+    p1.start()
+    p2 = Process(target=session2)
+    p2.start()
+    p1.join()
+    p2.join()
+    print('=============================================================')
 
 
 TRANSACTION_LVLS = [
@@ -136,19 +209,19 @@ TRANSACTION_LVLS = [
 def test_levels():
     ensure_tables()
     for level in TRANSACTION_LVLS:
-        ensure_data()
-        print('Level: %s' % level)
-        with get_connection() as con:
+        print('=============TRANSACTION LEVEL = %s ====================' % level)
+        con = get_connection()
+        with con:
             with con.cursor() as cursor:
                 cursor.execute(
                     'SET GLOBAL TRANSACTION ISOLATION LEVEL %s' % level)
 
             con.commit()
 
-        lost_update()
-        print_table()
-        # dirty_read()
-        # print_table()
+        for case in [lost_update, dirty_read, non_repeatable_read, phantom_read]:
+            ensure_data()
+            case()
+            print_table()
 
 
 test_levels()
