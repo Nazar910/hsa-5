0d4704c74298c3a0b55929eb154748ade47efca9
Nazarii Pyvovar <Nazar910>
2022-02-05 12:30:25 +0200

    L17: added counting_sort_diff

---
+++ L17/counting_sort_diff.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const { countingSort } = require('./counting_sort');
+const { measureExecutionNanoSec, gnuPlotCmd, randomNumber, clearFile } = require('./utils');
+
+const MEASUREMENTS_COUNT = 5_000;
+
+const TEST_CASE = 'counting_sort_diff';
+const SORT_DATA_FILE = `./results/${TEST_CASE}.dat`;
+
+clearFile(SORT_DATA_FILE);
+
+const sortStatsWriteStream = fs.createWriteStream(SORT_DATA_FILE);
+
+for (let i = 1; i < MEASUREMENTS_COUNT; i++) {
+    const size = i;
+
+    const input = Array.from(new Array(size)).map(() => randomNumber(size));
+
+    input.sort((a, b) => a - b);
+    input.push(1_000_000);
+
+    const sortEndTimeNanos = measureExecutionNanoSec(() => countingSort(input, 1, size));
+
+    sortStatsWriteStream.write(`${size} ${sortEndTimeNanos}\n`);
+}
+
+sortStatsWriteStream.end('', () => gnuPlotCmd(TEST_CASE, 1_000_000));
