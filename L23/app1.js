d4539cbda015bed8e5e402309e7aad9ec6cdfcbd
Nazarii Pyvovar <Nazar910>
2022-02-11 23:16:31 +0200

    L23: upd Readme

--- L23/app1.js
+++ L23/app1.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     switch (req.url) {
         case '/health':
-            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.writeHead(200);
             res.end();
             break;
         default:
